b10e90d8aa368fe92ce488cc0cc81d9ab1a3c9fa
Mihai H <mihaihonceriu@gmail.com>
2019-07-27 16:49:45 +0300

    Initial Commit

---
+++ Untind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Untind
+//
+//  Created by Honceriu Mihai on 27/07/2019.
+//  Copyright © 2019 FincPicsels. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UntindTests/UntindTests.swift
@@ -0,0 +1,34 @@
+//
+//  UntindTests.swift
+//  UntindTests
+//
+//  Created by Honceriu Mihai on 27/07/2019.
+//  Copyright © 2019 FincPicsels. All rights reserved.
+//
+
+import XCTest
+@testable import Untind
+
+class UntindTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
