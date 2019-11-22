96f865052c16aeeb09fe2461877816900c06d80f
Mihai Honceriu <mihai.honceriu@deindeal.ch>
2019-11-22 19:26:02 +0200

    New implementation for chat cells

--- Untind/Custom Views/Cells/Messages/MessageCell.swift
+++ Untind/Custom Views/Cells/Messages/MessageCell.swift
@@ -13,35 +13,69 @@ enum MessageCellSender {
     case other
 }
 class MessageCell: UICollectionViewCell {
-
-    @IBOutlet weak var messageView: UIView!
-    @IBOutlet weak var cellLabel: UILabel!
-    @IBOutlet var messageViewLeadingConstraint: NSLayoutConstraint!
-    @IBOutlet var messageViewTrailingConstraint: NSLayoutConstraint!
-    @IBOutlet var msgViewLeadingOptionalConstraint: NSLayoutConstraint!
-    @IBOutlet var msgViewTrailingOptionalConstraint: NSLayoutConstraint!
     
     public var sender : MessageCellSender = .me {
         didSet {
-            if sender == .me {
-                messageView.backgroundColor = UIColor.teal2
-                messageViewTrailingConstraint.isActive = true
-                messageViewLeadingConstraint.isActive = false
-            } else {
-                messageView.backgroundColor = UIColor.flatOrange
-                messageViewTrailingConstraint.isActive = false
-                messageViewLeadingConstraint.isActive = true
-            }
+           
         }
+    }
+    
+    let messageTextView : UITextView = {
+        let textView = UITextView()
+        textView.font = UIFont(name: "HelveticaNeue-Medium", size: 16)
+        textView.textColor = UIColor.white
+        textView.text = "Sample message"
+        textView.backgroundColor = UIColor.clear
+        textView.isUserInteractionEnabled = false
+        return textView
+    }()
+    
+    let textBubbleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.flatOrange
+        view.layer.cornerRadius = 18
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+           super.init(frame: frame)
+           setupViews()
+       }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupViews()
+    }
+    
+    func setupViews() {
+        addSubview(textBubbleView)
+        addSubview(messageTextView)
+    }
+    
+    func configureWithMessage(message: String, chatViewWidth: CGFloat, sender: MessageCellSender) {
+        messageTextView.text = message
+        
+        let size = CGSize(width: 250, height: 1000)
+        let options = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
+        let estimatedFrame = NSString(string: message).boundingRect(with: size, options: options, attributes: [NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-Medium", size: 16)!], context: nil)
+        
+        var x : CGFloat = 8
+        
+        if sender == .me {
+            x = chatViewWidth - estimatedFrame.width - 16 - 8 - 8
+            textBubbleView.backgroundColor = UIColor.flatOrange
+        } else {
+            textBubbleView.backgroundColor = UIColor.answerTeal
+        }
+        
+        self.messageTextView.frame = CGRect(x: x + 8, y: 0, width: estimatedFrame.width + 16, height: estimatedFrame.height + 20)
+        
+        self.textBubbleView.frame = CGRect(x: x, y: 0, width: estimatedFrame.width + 16 + 8, height: estimatedFrame.height + 20)
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        messageView.layer.cornerRadius = 16
     }
     
-    func configureWithMessage(message: String) {
-        cellLabel.text = message
-    }
-
 }
--- Untind/View Controllers/Chat/ChatViewController.swift
+++ Untind/View Controllers/Chat/ChatViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ChatViewController: UIViewController, UITextViewDelegate, UICollectionViewDelegate, UICollectionViewDataSource {
+class ChatViewController: UIViewController, UITextViewDelegate, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UIScrollViewDelegate {
     @IBOutlet weak var bottomChatViewHeight: NSLayoutConstraint!
     @IBOutlet weak var messageTextView: UITextView!
     @IBOutlet weak var messageTextViewBackground: UIView!
     @IBOutlet weak var bottomInputViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var messageInputViewBottomConstraint: NSLayoutConstraint!
     @IBOutlet weak var bottomInputView: UIView!
     @IBOutlet weak var placeHolderLabel: UILabel!
     @IBOutlet weak var sendButton: UIButton!
@@ -32,30 +33,53 @@ class ChatViewController: UIViewController, UITextViewDelegate, UICollectionView
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        messageList = ["Hello. I have sent you a message to let you know about our meeting next week. We should find some time to meet before friday, if possible", "Hey, sure let me check up my schedule", "Okay", "What about monday? I have a few meetings in the morning, but i think i can squeeze this one in." , "Sounds nice. Dinner?", "Yup"]
         
         messageTextViewBackground.layer.borderWidth = 1
         messageTextViewBackground.layer.cornerRadius = 18
         messageTextViewBackground.layer.borderColor = UIColor.flatOrange.cgColor
         messageTextViewBackground.backgroundColor = UIColor(white: 1, alpha: 0.5)
         
-        messagesCollectionView.register(UINib(nibName: "MessageCell", bundle: nil), forCellWithReuseIdentifier: "MessageCell")
-        if let flowLayout = messagesCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
-            flowLayout.estimatedItemSize = CGSize(width: self.view.bounds.size.width, height: 50)
-        }
-        
+        messagesCollectionView.register(MessageCell.self, forCellWithReuseIdentifier: "MessageCell")
         messagesCollectionView.delegate = self
         messagesCollectionView.dataSource = self
         
         messageTextView.delegate = self
+        
 
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        //tap.cancelsTouchesInView = false
+
+        messagesCollectionView.addGestureRecognizer(tap)
+        
+    }
+    
+    //Calls this function when the tap is recognized.
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    func resetMessageTextView() {
+        if messageTextView.text != "" {
+            placeHolderLabel.isHidden = true
+            sendButton.isHidden = false
+        } else {
+            placeHolderLabel.isHidden = false
+            sendButton.isHidden = true
+        }
     }
     
     
     @IBAction func sendButtonTapped(_ sender: Any) {
-        self.messageTextView.resignFirstResponder()
         messageList.append(self.messageTextView.text)
         self.messagesCollectionView.insertItems(at: [IndexPath(item: messageList.count-1, section: 0)])
         self.messageTextView.text = ""
+        self.messagesCollectionView.scrollToItem(at: IndexPath(item: messageList.count-1, section: 0), at: .top, animated: true)
+        messageTextView.resignFirstResponder()
     }
     
     @IBAction func backButtonTapped(_ sender: Any) {
@@ -63,39 +87,55 @@ class ChatViewController: UIViewController, UITextViewDelegate, UICollectionView
     }
     
     //MARK: - Keyboard Handling
-       @objc func keyboardWillShow(_ notification: Notification) {
-           if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
-                  let keyboardRectangle = keyboardFrame.cgRectValue
-                  let keyboardHeight = keyboardRectangle.height
-                  bottomInputView.transform = CGAffineTransform(translationX: 0, y: -keyboardHeight+20)
-              }
-       }
-       
-       @objc func keyboardWillHide(_ notification: Notification) {
-           bottomInputView.transform = CGAffineTransform.identity
-       }
+    @objc func keyboardWillShow(_ notification: Notification) {
+        
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+            
+            messageInputViewBottomConstraint.constant = keyboardHeight + 20
+            
+            UIView.animate(withDuration: 0, animations: {
+                self.view.layoutIfNeeded()
+            }) { (completed) in
+                self.messagesCollectionView.scrollToItem(at: IndexPath(item: self.messageList.count-1, section: 0), at: .bottom, animated: true)
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        messageInputViewBottomConstraint.constant = 0
+        
+        UIView.animate(withDuration: 0, animations: {
+            self.view.layoutIfNeeded()
+        })
+    }
     
     //MARK: - UITextView Delegate
-  
+    
     func textViewDidChange(_ textView: UITextView) {
-        if textView == messageTextView {
-            if textView.text != "" {
-                placeHolderLabel.isHidden = true
-                sendButton.isHidden = false
-            } else {
-                placeHolderLabel.isHidden = false
-                sendButton.isHidden = true
-            }
-            
-            bottomInputViewHeight.constant = min(max(30,textView.contentSize.height),120)
-        }
+        resetMessageTextView()
     }
     
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         return true
     }
+    // MARK: - ScrollViewDelegate
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+
+    }
     
     // MARK: - CollectionViewDelegate
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        
+        let messageText = messageList[indexPath.item]
+        let size = CGSize(width: 250, height: 1000)
+        let options = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
+        let estimatedFrame = NSString(string: messageText).boundingRect(with: size, options: options, attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18)], context: nil)
+        
+        return CGSize(width: view.frame.width, height: estimatedFrame.height + 20)
+    }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
@@ -107,13 +147,7 @@ class ChatViewController: UIViewController, UITextViewDelegate, UICollectionView
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MessageCell", for: indexPath) as! MessageCell
-        cell.configureWithMessage(message: messageList[indexPath.row])
-        
-        if indexPath.row % 2 == 0 {
-            cell.sender = .other
-        } else {
-            cell.sender = .me
-        }
+        cell.configureWithMessage(message: messageList[indexPath.row], chatViewWidth: messagesCollectionView.frame.size.width, sender: indexPath.row % 2 == 0 ? .me : .other)
         
         return cell
         
